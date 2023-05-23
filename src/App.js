b91c6e2e1dfedc6c4d59da59eeeb5b33dc2d6b30
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-05-23 18:58:49 +0200

    ajout IncListing + NavBar sur toute les pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Navbar from  "./Components/Navbar";
+import IncListing from  "./Components/IncListing";
 import './App.css';
 import {Routes,Route} from "react-router-dom"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navbar/>} />
-    </Routes>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Navbar/>} />
+        <Route path="/Incident" element={<IncListing/>} />
+      </Routes>
+    </div>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
             <h3>Logo</h3>
             <nav ref={navRef}>
                 <a href="/#">Home</a>
-                <a href="/#">My work</a>
+                <a href="/Incident">Incident</a>
                 <a href="/#">Blog</a>
                 <a href="/#">About Me</a>
                 <button className="nav-btn nav-close-btn" onClick={showNavbar}>
